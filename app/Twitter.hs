ba1bac10a58f668021bf17e38defce938640c64f
ryanbas21 <ryanbas21@gmail.com>
2020-10-17 21:56:39 -0600

    created api request for webhook

--- app/Twitter.hs
+++ app/Twitter.hs
@@ -1,31 +1,33 @@
+{-# LANGUAGE DataKinds #-}
 -- allows "string literals" to be Text
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Twitter (twitter) where
+module Twitter (twitter, webhook) where
 
 import Configuration.Dotenv (defaultConfig, loadFile)
 import Control.Monad.Reader
 import qualified Data.ByteString.Char8 as S8
-import System.Environment
+import System.Environment (getEnv)
 import Web.Authenticate.OAuth as OA
 import Web.Twitter.Conduit
-    ( Manager,
-      newManager,
-      tlsManagerSettings,
-      usersShow,
-      call,
-      setCredential,
-      twitterOAuth,
-      UserParam(ScreenNameParam),
-      TWInfo(twProxy) )
+  ( APIRequest (..),
+    Manager,
+    TWInfo (twProxy),
+    UserParam (ScreenNameParam),
+    call,
+    newManager,
+    setCredential,
+    tlsManagerSettings,
+    twitterOAuth,
+    usersShow,
+  )
 import Web.Twitter.Types (User)
 
 getTokens :: IO OAuth
 getTokens = do
   consumerKey <- getEnv "OAUTH_CONSUMER_KEY"
   consumerSecret <- getEnv "OAUTH_CONSUMER_SECRET"
-  -- accessToken <- getEnv "TWITTER_BEARER_TOKEN"
   return $
     twitterOAuth
       { oauthConsumerKey = S8.pack consumerKey,
@@ -45,19 +47,29 @@ getScreenName :: IO TWInfo -> IO User
 getScreenName info = join $ call <$> info <*> manager <*> (pure $ usersShow $ ScreenNameParam "ryanbas21")
 
 credential :: IO Credential
-credential = do 
+credential = do
   oauth_token <- S8.pack <$> getEnv "TWITTER_ACCESS_TOKEN"
   oauth_secret <- S8.pack <$> getEnv "TWITTER_ACCESS_SECRET"
-  pure $ Credential
-    [ ("oauth_token", oauth_token)
-    , ("oauth_token_secret", oauth_secret)
-    ]
+  pure $
+    Credential
+      [ ("oauth_token", oauth_token),
+        ("oauth_token_secret", oauth_secret)
+      ]
+
+webhookRequest :: String -> APIRequest '[] [String]
+webhookRequest env = APIRequest "GET" ("https://api.twitter.com/1.1/account_activity/all/" ++ env ++ "/webhooks.json") []
+
+webhook :: IO [String]
+webhook = do
+  env <- getEnv "TWITTER_WEBHOOK_ENV"
+  info <- twInfo <$> credential
+  join $ call <$> info <*> manager <*> (pure $ webhookRequest env)
 
 twitter :: IO ()
 twitter = do
   loadFile defaultConfig
-  info <- twInfo <$> credential 
-  req <- getScreenName info
+  -- info <- twInfo <$> credential
+  -- req <- getScreenName info
+  req <- webhook
   print req
   pure ()
-
